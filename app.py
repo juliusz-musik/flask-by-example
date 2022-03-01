6f016750450ce3aa3c3d38005b54a1488d0dc69e
Juliusz Musik <juliusz.musik@motorolasolutions.com>
2022-03-01 11:50:06 +0100

    add config support

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
+app.config.from_object(env_config)
+
 
 @app.route("/")
 def index():
-    return "hello app"
+    secret_key = app.config.get("SECRET_KEY")
+    return f"hello app, my secret is {secret_key}"
