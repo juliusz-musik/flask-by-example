16a14dc01035bb4a6ab18fc85c507265e462a0ae
Juliusz Musik <juliusz.musik@motorolasolutions.com>
2022-03-08 11:41:46 +0100

    add db and migrations

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import os
 from flask import Flask
+from flask_migrate import Migrate
+
+from models import db, Result
 
 app = Flask(__name__)
-env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
-app.config.from_object(env_config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(os.getenv("APP_SETTINGS", "config.DevelopmentConfig"))
+
+db.init_app(app)
+Migrate(app, db)
 
 
 @app.route("/")
 def index():
     secret_key = app.config.get("SECRET_KEY")
     return f"hello app, my secret is {secret_key}"
+
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
+
+if __name__ == '__main__':
+    app.run()
--- config.py
+++ config.py
@@ -1,16 +1,30 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     DEBUG = False
     DEVELOPMENT = False
+    TESTING = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 
 class ProductionConfig(Config):
     pass
 
+
 class StagingConfig(Config):
     DEBUG = True
+    DEVELOPMENT = True
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
     DEVELOPMENT = True
+
+
+class TestingConfig(Config):
+    TESTING = True
