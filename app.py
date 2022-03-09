ae4d9641acae105d168088df2b7e33998582b4f5
Juliusz Musik <juliusz.musik@motorolasolutions.com>
2022-03-09 17:59:41 +0100

    word count initial

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import os
-from flask import Flask
+import requests
+import operator
+import re
+import nltk
+from flask import Flask, render_template, request
 from flask_migrate import Migrate
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
+
 
 from models import db, Result
 
@@ -12,15 +20,48 @@ db.init_app(app)
 Migrate(app, db)
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    secret_key = app.config.get("SECRET_KEY")
-    return f"hello app, my secret is {secret_key}"
+    errors = []
+    results = {}
+    r = None
+    if request.method == 'POST':
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append("Unable to get url.")
+        if r:
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk.data.path.append('./nltk_data/')  # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            # save the results
+            results = sorted(
+                no_stop_words_count.items(),
+                key=operator.itemgetter(1),
+                reverse=True
+            )
+            try:
+                result = Result(
+                    url=url,
+                    result_all=raw_word_count,
+                    result_no_stop_words=no_stop_words_count
+                )
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
 
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
